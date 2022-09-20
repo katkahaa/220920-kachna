8802dac3cf50e5fdcae84380966ba9a2383fe819
haasova.ka.2019 <haasova.ka.2019@UC313PC05>
2022-09-20 15:11:17 +0200

    Přidejte soubory projektu.

---
+++ 220920 kachna/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _220920_kachna
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static bool IsPrimeNumber(int number)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < number/2; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return isPrime;
+
+        }
+
+        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+
+   
+}
